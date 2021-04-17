6d92e362955703ef1d8909c998b674145617d491
Delessar <daniel.edery@hotmail.com>
2021-04-17 16:36:28 -0400

    fix for taken tokens

--- PNT_Player.py
+++ PNT_Player.py
@@ -8,12 +8,13 @@ def main():
 
     tokens = list(range(1, int(sys.argv[1]) + 1))
     taken_tokens_integer = int(sys.argv[2])
+    print(taken_tokens_integer)
     taken_tokens = []
     depth = None if int(sys.argv[3 + taken_tokens_integer]) == 0 else int(sys.argv[3 + taken_tokens_integer])
 
     if taken_tokens_integer != 0:
-        for i in range(0, taken_tokens_integer - 1):
-            x = int(sys.argv[taken_tokens_integer + i])
+        for i in range(0, taken_tokens_integer):
+            x = int(sys.argv[taken_tokens_integer+1 + i])
             taken_tokens.append(x)
             tokens.remove(x)
 
@@ -22,9 +23,11 @@ def main():
     #player = False
     #evaluation = 0
     #depth = None
+    print(tokens)
+    print(taken_tokens)
     while not done:
         player = len(taken_tokens) % 2 == 0
-        evaluation, move = play_PNT(tokens, taken_tokens, depth, float('-inf'), float('inf'), player)
+        evaluation, move = play_PNT(tokens, taken_tokens, depth, float('-1.0'), float('1.0'), player)
         if depth is not None:
             depth = depth - 1
         if move is None:
